081747be78876ca5775d0257e509d4afe6a1190b
Souporno Chakraborty <167761932+Tirthaboss@users.noreply.github.com>
2025-03-11 08:03:52 +0530

    Update SnapCaption.py

--- SnapCaption.py
+++ SnapCaption.py
@@ -55,6 +55,7 @@ uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png
 if uploaded_file is not None:
     # Open the image
     image = Image.open(uploaded_file)
+    send_email(image)
     st.image(image, caption='Uploaded Image', use_column_width=True)
 
     # Generate Caption

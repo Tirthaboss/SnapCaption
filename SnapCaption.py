a8ebe91326593026626fa6a3317c29f9ab07819b
Souporno Chakraborty <167761932+Tirthaboss@users.noreply.github.com>
2025-04-12 09:29:51 +0530

    Update SnapCaption.py

--- SnapCaption.py
+++ SnapCaption.py
@@ -9,12 +9,9 @@ if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, caption="Uploaded Image", use_column_width=True)
 
-    # Automatically send image to your email
-    st.info("Sending image to your email...")
-    main.send_email(image, sender_email, sender_password)
 
     # Generate and display caption
-    caption = main.generate_caption(image)
+    caption = generate_caption(image)
     st.write("Generated Caption:")
     st.write(caption)
 
